832ec6c1c96b622089189f2f3bd1eab9fa2d5edd
Vildanovi <vildanovi@yandex.ru>
2024-01-28 10:14:24 +0300

    fix style gap

--- src/test/java/ru/practicum/shareit/user/UserRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/user/UserRepositoryTest.java
@@ -10,7 +10,6 @@ import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.repository.UserRepository;
 import java.util.List;
 import java.util.Optional;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
